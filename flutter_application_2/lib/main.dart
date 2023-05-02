05489d9c880e65b06a6751717daa1da565215630
Nasko V <atanaskot@abv.bg>
2023-05-02 21:26:36 +0300

    ok

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,115 +1,248 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      title: 'My app', // used by the OS task switcher
+      home: SafeArea(
+        child: MyScaffold(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, super.key});
 
-  // This widget is the root of your application.
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: const BoxDecoration(color: Color.fromARGB(255, 53, 127, 223)),
+      // Row is a horizontal, linear layout.
+      child: Row(
+        children: [
+          Expanded(
+              child: Center(
+            child: title,
+          )),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    // Material is a conceptual piece
+    // of paper on which the UI appears.
+    return Material(
+      // Column is a vertical, linear layout.
+      child: Column(
+        children: [
+          MyAppBar(
+            title: Text(
+              'Calculator',
+              style: Theme.of(context) //
+                  .primaryTextTheme
+                  .titleLarge,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 366.0, right: 34.0),
+              child: Text(
+                '0',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            child: Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                  ElevatedButton(
+                    child: Text(
+                      'C',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '±',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '%',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '÷',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  )
+                ])),
+          ),
+          Container(
+            child: Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                  ElevatedButton(
+                    child: Text(
+                      '7',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '8',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '9',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      'x',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  )
+                ])),
+          ),
+          Container(
+            child: Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                  ElevatedButton(
+                    child: Text(
+                      '4',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '5',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '6',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '-',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  )
+                ])),
+          ),
+          Container(
+            child: Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                  ElevatedButton(
+                    child: Text(
+                      '1',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '2',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '3',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '+',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  )
+                ])),
+          ),
+          Container(
+            child: Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                  ElevatedButton(
+                    child: Text(
+                      '0',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      ',',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      '=',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    onPressed: () {},
+                  )
+                ])),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
